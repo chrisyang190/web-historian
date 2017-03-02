fd0dceef8dd78042ef3321ae439ce25859f544ec
Christopher Yang <chrisyang190@gmail.com>
2017-03-02 01:19:34 -0800

    Refactored to Pass Tests

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -20,29 +20,30 @@ exports.serveAssets = function(res, asset, callback) {
 
 
 // As you progress, keep thinking about what helper functions you can put here!
-exports.createResponse = function(path, file, res) {
+exports.createGetResponse = function(path, file, res) {
   fs.readFile(path + '/' + file, function(error, content) {
     if (error) {
+      console.log('error');
       res.writeHead(404);
       res.end();
     } else {
-      archive.isUrlArchived(file, function(bool) {
-        if (bool) {
+      archive.isUrlArchived(file, function(exists) {
+        if (exists) {
           // console.log('The 200 if block causing this');
           res.writeHead(200, {'Content-type': 'text/html'});
           res.end(content, 'utf-8');
         } else {
-          console.log(file);
           if (file === 'index.html') {
-            console.log('post????');
+            // console.log('post????');
             res.writeHead(200, {'Content-type': 'text/html'});
             res.end(content, 'utf-8');
-          } else {
-            console.log('in 302');
-            archive.addUrlToList(file, function(url) { console.log(url); } );
-            res.writeHead(302, {'Content-type': 'text/html'});
-            res.end(content, 'utf-8');
-          }
+          } 
+          // else if (method === 'POST') {
+          //   archive.addUrlToList(file, function(url) { console.log(url); } );
+          //   console.log('file in 302', file);
+          //   res.writeHead(302, {'Content-type': 'text/html'});
+          //   res.end(content, 'utf-8');
+          // }
         }
 
 
@@ -60,3 +61,36 @@ exports.createResponse = function(path, file, res) {
     }
   });
 };
+
+exports.createPostResponse = function(path, file, req, res) {
+  // console.log('p1', path);
+  // fs.readFile(path, function(error, content) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+      // console.log('file!!', file);
+  var string = '';
+  req.on('data', function(data) {
+    string += data;
+    string = string.slice(4) + '\n';
+  }).on('end', function() {
+    archive.isUrlInList(string, function(exists) {
+      console.log('string:', string);
+      if (!exists) {
+        archive.addUrlToList(string, function(url) { console.log(url); } );
+        // console.log('file in 302', file);
+        res.writeHead(302, {'Content-type': 'text/html'});
+        res.end('content', 'utf-8');
+      } else {
+        res.writeHead(302, {'Content-type': 'text/html'});
+        res.end('content', 'utf-8');
+      }
+    });
+  });
+  //   }
+  // });
+
+};
+
+
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -6,9 +6,34 @@ var fs = require('fs');
 
 
 exports.handleRequest = function (req, res) {
-  if (req.url.slice(1)) {
-    httpHelpers.createResponse(archive.paths.archivedSites, req.url.slice(1), res);
-  } else {
-    httpHelpers.createResponse(archive.paths.siteAssets, 'index.html', res);
+  if (req.method === 'GET') {
+    if (req.url.slice(1)) {
+      httpHelpers.createGetResponse(archive.paths.archivedSites, req.url.slice(1), res);
+    } else {
+      httpHelpers.createGetResponse(archive.paths.siteAssets, 'index.html', res);
+    }
   }
+
+  if (req.method === 'POST') {
+    httpHelpers.createPostResponse(archive.paths.list, req.url.slice(1), req, res);
+  }
+
+// if method is GET  
+  // if it on list
+    // if it is archived
+      // use 200, archive.paths.archivedSites + req.url 
+    // else if not archived, 
+      // use 200 archive.paths.archivedSites + ./loading.html
+  // else use 200, archive.paths.siteAssets + index.html
+  // else 404
+
+// else if method is POST
+  // if NOT on the list
+    // if it is archived
+        // use 302, archive.paths.archivedSites + req.url 
+      // else if not archived, archive.paths.archivedSites + ./loading
+        // 
+    // else use 302, archive.paths.siteAssets + index.html
+
+
 };
